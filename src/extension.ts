9f43590dc077a0613990c71db3de8c16a193bb96
Daniel Federschmidt <dfederschmidt@splunk.com>
2022-05-06 11:54:09 +0200

    remove unused imports and actually dispose the deploytaskprovider

--- src/extension.ts
+++ src/extension.ts
@@ -1,19 +1,10 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-import { openWebActionRunResult, openWebPlaybook } from './commands/web'
-import { DeployTaskProvider } from './tasks/deployTaskProvider';
-import { runActionInput } from './commands/apps/runAction';
-import { viewAppDocs } from './commands/apps/viewAppDocs';
-import { repeatActionRun } from './commands/actionRuns/repeatActionRun';
-import { getClientForActiveEnvironment } from './soar/client';
-import {AppWizardPanel} from './webviews/appWizard'
-import { RunActionLensProvider, runActionLensSelector } from './codelens/runActionLensProvider';
 import { registerCommands } from './commands/commands';
 import { registerTreeViews } from './views/views';
 import { registerInspectProviders } from './inspect/inspect';
 import { registerCodeLenses } from './codelens/codelens';
+import { DeployTaskProvider } from './tasks/deployTaskProvider';
 
 let deployTaskProvider: vscode.Disposable | undefined;
 
@@ -31,10 +22,9 @@ export function activate(context: vscode.ExtensionContext) {
 	}
 
 	// Task Provider
-	let deployTaskProvider = vscode.tasks.registerTaskProvider(DeployTaskProvider.CustomBuildScriptType, new DeployTaskProvider(rootPath, context));
+	deployTaskProvider = vscode.tasks.registerTaskProvider(DeployTaskProvider.CustomBuildScriptType, new DeployTaskProvider(rootPath, context));
 }
 
-// this method is called when your extension is deactivated
 export function deactivate() {
 	if (deployTaskProvider) {
 		deployTaskProvider.dispose();
